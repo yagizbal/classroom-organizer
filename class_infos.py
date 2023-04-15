ac3a5715834db061f5fdff0a824d72e95e479527
YagizBal <64539817+yagizbal@users.noreply.github.com>
2023-04-15 20:49:33 +0200

    Add files via upload

---
+++ class_infos.py
@@ -0,0 +1,13 @@
+class_infos = [
+    ["Bioinformatics", "Chemistry", "Physics", "Calculus", "Biology", "ComputerScience",
+     "LinearAlgebra", "Statistics", "Psychology", "Economics", "Literature", "History"],
+    [45, 60, 45, 55, 60, 45, 45, 55, 30, 45, 60, 30],
+    ["Science", "Science", "Science", "Mathematics", "Science", "Engineering",
+     "Mathematics", "Mathematics", "SocialSciences", "SocialSciences", "Humanities", "Humanities"]
+     ,[6, 8, 7, 7, 6, 5, 6, 7, 4, 5, 8, 4]
+
+]
+#list 1 holds the names of the classes
+#list 2 holds the number of hours for each class, 
+#list 3 holds the department for each class, 
+#list 4 holds the number of credits for each class
