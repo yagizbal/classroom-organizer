1f3691753d556793f8698e0656c9d43e4106aad1
YagizBal <64539817+yagizbal@users.noreply.github.com>
2023-05-03 23:46:32 +0200

    Add files via upload

--- class_infos.py
+++ class_infos.py
@@ -1,13 +1,47 @@
-class_infos = [
-    ["Bioinformatics", "Chemistry", "Physics", "Calculus", "Biology", "ComputerScience",
-     "LinearAlgebra", "Statistics", "Psychology", "Economics", "Literature", "History"],
-    [45, 60, 45, 55, 60, 45, 45, 55, 30, 45, 60, 30],
-    ["Science", "Science", "Science", "Mathematics", "Science", "Engineering",
-     "Mathematics", "Mathematics", "SocialSciences", "SocialSciences", "Humanities", "Humanities"]
-     ,[6, 8, 7, 7, 6, 5, 6, 7, 4, 5, 8, 4]
-
-]
-#list 1 holds the names of the classes
-#list 2 holds the number of hours for each class, 
-#list 3 holds the department for each class, 
-#list 4 holds the number of credits for each class
+faculties = {
+    "Chemistry": {
+        "lecturers": ["Dr. Adams", "Dr. Wilson", "Dr. Baker"],
+        "lecture_halls": ["Chem Hall 1", "Chem Hall 2", "Chem Hall 3"],
+        "subjects": {
+            "Analytical Chemistry": ["Dr. Adams", "Dr. Wilson"],
+            "Organic Chemistry": ["Dr. Baker"],
+            "Physical Chemistry": ["Dr. Adams"]
+        }
+    },
+    "Mathematics": {
+        "lecturers": ["Dr. Smith", "Dr. Johnson", "Dr. Lee"],
+        "lecture_halls": ["Math Hall 1", "Math Hall 2", "Math Hall 3"],
+        "subjects": {
+            "Calculus": ["Dr. Smith", "Dr. Lee"],
+            "Linear Algebra": ["Dr. Johnson"],
+            "Probability and Statistics": ["Dr. Smith"]
+        }
+    },
+    "Informatics": {
+        "lecturers": ["Dr. Brown", "Dr. Davis", "Dr. Stewart"],
+        "lecture_halls": ["Info Hall 1", "Info Hall 2", "Info Hall 3"],
+        "subjects": {
+            "Data Structures": ["Dr. Brown", "Dr. Davis"],
+            "Algorithms": ["Dr. Stewart"],
+            "Computer Networks": ["Dr. Brown"]
+        }
+    },
+    "Biology": {
+        "lecturers": ["Dr. Taylor", "Dr. Miller", "Dr. White"],
+        "lecture_halls": ["Bio Hall 1", "Bio Hall 2", "Bio Hall 3"],
+        "subjects": {
+            "Cell Biology": ["Dr. Taylor"],
+            "Genetics": ["Dr. Miller", "Dr. White"],
+            "Metabolism of Proteins": ["Dr. Taylor", "Dr. White"]
+        }
+    },
+    "Biotechnology": {
+        "lecturers": ["Dr. Clark", "Dr. Lewis", "Dr. Hall"],
+        "lecture_halls": ["Biotech Hall 1", "Biotech Hall 2", "Biotech Hall 3"],
+        "subjects": {
+            "Bioprocessing": ["Dr. Clark", "Dr. Lewis"],
+            "Genetic Engineering": ["Dr. Hall"],
+            "Bioinformatics": ["Dr. Clark"]
+        }
+    }
+}
